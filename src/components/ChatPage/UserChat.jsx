51221d64581cc9ac010c37c16d29c10e050557c6
장서현 <jsh030213@gmail.com>
2025-04-12 01:08:54 +0900

    style: userChat padding 20px로 수정

--- src/components/ChatPage/UserChat.jsx
+++ src/components/ChatPage/UserChat.jsx
@@ -3,8 +3,8 @@ const UserChat = ({ sessionMessages }) => {
   return (
     <>
       <div className="flex justify-end">
-        <div className="bg-gray-stroke03 font-normal text-gray-stroke70 pl-[18px] pr-[16px] py-[16px] rounded-[30px] max-w-[70%] whitespace-pre-line break-words">
-          안녕하세요
+        <div className="bg-gray-stroke03 font-normal text-gray-stroke70 pl-[18px] pr-[16px] py-[16px] rounded-[20px] max-w-[70%] whitespace-pre-line break-words">
+          ㅎㅇ
         </div>
       </div>
     </>
