9eccd4e511fac91cf11ebc7b1f713386c2e74834
장서현 <jsh030213@gmail.com>
2025-04-12 02:25:01 +0900

    feat: 자동 스크롤 기능 추가

--- src/components/ChatPage/ChatMessageList.jsx
+++ src/components/ChatPage/ChatMessageList.jsx
@@ -1,8 +1,14 @@
-// 채팅을 리스트로 감싸기 (type을 보고 user, bot 챗 렌더링)
-import UserChat from './UserChat';
+import { useEffect, useRef } from 'react';
 import BotChat from './BotChat';
+import UserChat from './UserChat';
 
 const ChatMessageList = ({ sessionMessages }) => {
+  const bottomRef = useRef(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [sessionMessages]);
+
   return (
     <div className="flex flex-col">
       {sessionMessages.map((item, idx) => {
@@ -20,9 +26,12 @@ const ChatMessageList = ({ sessionMessages }) => {
         ) : (
           <div>
             <BotChat key={idx} message={item.message} />
+            {/* 확인용 코드 추후 삭제 예정 */}
           </div>
         );
       })}
+      {/* ⬇️ 맨 아래 요소 */}
+      <div ref={bottomRef} />
     </div>
   );
 };
