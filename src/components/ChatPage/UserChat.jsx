f498975a155155661a222790ab960b82f73d263c
장서현 <jsh030213@gmail.com>
2025-04-11 22:35:43 +0900

    feat: userChat UI 디자인 구현

--- src/components/ChatPage/UserChat.jsx
+++ src/components/ChatPage/UserChat.jsx
@@ -1,6 +1,14 @@
 // 사용자 채팅
-const UserChat = () => {
-  return <div>UserChat</div>;
+const UserChat = ({ sessionMessages }) => {
+  return (
+    <>
+      <div className="flex justify-end">
+        <div className="bg-gray-stroke03 font-normal text-gray-stroke70 pl-[18px] pr-[16px] py-[16px] rounded-[30px] max-w-[70%] whitespace-pre-line break-words">
+          안녕하세요
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserChat;
