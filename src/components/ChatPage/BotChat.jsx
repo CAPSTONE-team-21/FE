cb87928ffd283dbe15bbe332e52d58e2413dd6d0
장서현 <jsh030213@gmail.com>
2025-04-12 00:38:12 +0900

    refactor: 봇메세지 mockData 추가

--- src/components/ChatPage/BotChat.jsx
+++ src/components/ChatPage/BotChat.jsx
@@ -1,5 +1,18 @@
 import BotChatContainer from './BotChatContainer';
 // 챗봇 답변
+// 테스트용 응답 (백엔드 응답이 여기에 들어왔다고 가정)
+const botMessages = [
+  {
+    sender: 'BOT',
+    skinType: 'SENSITIVE',
+    message: '민감성 피부에게는 자극이 될 수 있습니다.',
+  },
+  {
+    sender: 'BOT',
+    skinType: 'DRY',
+    message: '건성 피부에게는 괜찮은 성분입니다.',
+  },
+];
 const BotChat = ({ sessionMessages }) => {
   return (
     <section className="">
