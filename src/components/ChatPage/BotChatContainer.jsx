65993061704f866dbde638d52158fc9674d4f38a
장서현 <jsh030213@gmail.com>
2025-04-11 23:41:46 +0900

    feat: BotChat 응답의 피부 타입 선택 UI 구현

--- src/components/ChatPage/BotChatContainer.jsx
+++ src/components/ChatPage/BotChatContainer.jsx
@@ -3,7 +3,51 @@ import BotChat from './BotChat';
 // 선택한 피부 타입 필터링 용 컨테이너
 
 const BotChatContainer = () => {
-  return <></>;
+  return (
+    <div className="bg-main-buttonFill p-[4px] rounded-t-[15px] w-full mx-auto border-[1px] border-main-typeStroke">
+      {/* 필터 버튼 */}
+      <div className="flex gap-4">
+        <button
+          className="
+    basis-1/4 flex items-center justify-center
+    py-1
+    text-[14px] text-main
+    rounded-[10px]"
+        >
+          건성
+        </button>
+        <button
+          className="
+    basis-1/4 flex items-center justify-center
+    py-1
+    text-[14px] text-main-buttonStroke
+    hover:bg-main-buttonFill hover:text-main-buttonHover duration-200
+    focus:bg-white focus:text-main 
+    rounded-[10px]"
+        >
+          지성
+        </button>
+        <button
+          className="
+    basis-1/4 flex items-center justify-center
+    py-1
+    text-[14px] text-main-buttonStroke
+    rounded-[10px]"
+        >
+          민감성
+        </button>
+        <button
+          className="
+    basis-1/4 flex items-center justify-center
+    py-1
+    text-[14px] text-main-buttonStroke
+    rounded-[10px]"
+        >
+          복합성
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BotChatContainer;
