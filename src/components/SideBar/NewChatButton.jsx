b146021e1c7daeb3d242b344e8b799c652bf6a38
mikyung <seomk08@gmail.com>
2025-04-12 04:26:37 +0900

    feat: NewChatButton 기능 구현 및 호버 시 커서바뀜 추가

--- src/components/SideBar/NewChatButton.jsx
+++ src/components/SideBar/NewChatButton.jsx
@@ -1,15 +1,37 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ChatContext } from '../../contexts/ChatContext';
 import { IconPlus } from '../../utils/icons';
 
 const NewChatButton = () => {
+  const { createChatSession, setSidebarOpen } = useContext(ChatContext);
+  const navigate = useNavigate();
+
+  const handleNewChat = () => {
+    // 새로운 임시 세션 생성 (백 response 대용)
+    createChatSession({
+      mode: 'product_detail',
+      skin_type: '건성',
+    });
+
+    // 사이드바 닫음
+    setSidebarOpen(false);
+
+    // 메인 채팅 입력 화면으로 이동
+    navigate('/chat');
+  };
+
   return (
     <div
-      className="flex w-[220px] h-[38px] px-[10px] py-[8px] 
-    rounded-[15px] gap-[12px] text-main
-    hover:bg-main-newChatHover "
+      className="flex w-[220px] h-[38px] px-[10px] py-[8px]
+        rounded-[15px] gap-[12px] text-main
+        hover:bg-main-newChatHover cursor-pointer"
+      onClick={handleNewChat}
     >
       <img className="w-[20px] h-[20px]" src={IconPlus} alt="채팅추가버튼" />
       <div className="font-bold">새 채팅</div>
     </div>
   );
 };
+
 export default NewChatButton;
