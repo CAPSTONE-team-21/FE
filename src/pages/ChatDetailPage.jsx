aa23f77b9454683a2d42fa30b63aadfd96708579
장서현 <jsh030213@gmail.com>
2025-04-11 22:36:46 +0900

    Style: ChatDetailPage 디자인 수정

--- src/pages/ChatDetailPage.jsx
+++ src/pages/ChatDetailPage.jsx
@@ -7,16 +7,18 @@ import ChatInputBox from '../components/ChatPage/ChatInputBox';
 const ChatDetailPage = () => {
   return (
     <ChatProvider>
-      <div className="flex flex-col h-screen w-full bg-white">
-        <Header />
-
-        {/* 채팅 메시지 영역 - 입력창 공간 확보 위해 pb-[100px] */}
-        <div className="flex-1 flex justify-center overflow-y-auto pt-10 pb-[100px]">
-          <div className="w-[760px]">
+      <Header />
+      {/* 헤더: 고정 높이 */}
+      {/* 본문 영역: Header 제외하고 나머지 전체 사용 */}
+      <div className="relative h-screen flex-1">
+        {/* 채팅 메시지 영역 */}
+        <div className="flex justify-center h-full overflow-y-auto py-[60px]">
+          <div className="w-[760px] ">
             <ChatSection />
           </div>
         </div>
       </div>
+      <SideBar />
     </ChatProvider>
   );
 };
