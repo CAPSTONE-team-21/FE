8696eb8522532315eff5d97bfacda13230d96b5e
장서현 <jsh030213@gmail.com>
2025-04-28 19:03:05 +0900

    feat: api 주소 변경

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://api.sspoid.site',
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,
 });
 
