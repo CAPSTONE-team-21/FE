a87a6a0f9dee78081b4ac51d90cb3da70ff346ec
mikyung <seomk08@gmail.com>
2025-05-01 06:06:40 +0900

    chore: 목데이터수정

--- mocks/dummy/sessionMessagesMap.js
+++ mocks/dummy/sessionMessagesMap.js
@@ -1,7 +1,12 @@
 export const sessionMessagesMap = {
-  3: [
-    { sender: 'USER', skinType: 'DRY', message: '이 제품 써도 괜찮을까요?' },
-    { sender: 'BOT', skinType: 'DRY', message: '건성 피부라면 이 제품 괜찮습니다!' },
-  ],
+  1: [],
+  2: [],
+  3: [],
   4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+  10: [],
 };
