88426c2d9f03aa74b0c139f1187da4183b9173ae
mikyung <seomk08@gmail.com>
2025-04-30 14:50:03 +0900

    chore: api base주소생성 localhost8080

---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
+  withCredentials: true, // 쿠키 등 필요 시
+});
+
+export default api;
