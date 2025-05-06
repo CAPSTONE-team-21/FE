14a3bfea59cee11a8d43d328e10e7fbee01f9630
장서현 <jsh030213@gmail.com>
2025-05-06 21:53:29 +0900

    style: 챗봇 답변 디자인 수정 (1)

--- src/components/ChatPage/BotChatContainer.jsx
+++ src/components/ChatPage/BotChatContainer.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import { IconLogo } from '../../utils/icons';
 
 const SkinTypeLabel = {
   DRY: '건성',
@@ -35,6 +36,15 @@ const BotChatContainer = ({ botMessages }) => {
 
   return (
     <div className="flex flex-col w-full">
+      {/* 스포이드 추출 결과 안내 문구 */}
+      <div className="flex items-center mb-4">
+        <img className="w-7" src={IconLogo} alt="" />
+        <span className="ml-2 text-15 font-semibold bg-gradient-to-r from-main to-main-purple bg-clip-text text-transparent">
+          스포이드 추출 결과는 다음과 같습니다.
+        </span>
+      </div>
+
+      {/* 피부 타입 선택 */}
       <div className="relative flex w-full bg-main-2 rounded-t-[10px]">
         {botMessages.map((msg, idx) => (
           <button
@@ -61,12 +71,27 @@ const BotChatContainer = ({ botMessages }) => {
         />
       </div>
 
-      <div className="bg-white border-b-2 border-main-typeStroke font-normal text-gray-stroke70 px-[20px] py-[36px] max-w-[100%] whitespace-pre-line break-words leading-[1.4]">
-        {botMessages
-          .filter((msg) => msg.skinType === activeType)
-          .map((msg, idx) => (
-            <div key={idx}>{msg.message}</div>
-          ))}
+      <div className="group flex flex-col w-full">
+        {/* 챗 답변 렌더링 구간 */}
+        <div className="bg-white font-normal text-gray-stroke70 px-[20px] py-[36px] max-w-[100%] whitespace-pre-line break-words leading-[1.4]">
+          {botMessages
+            .filter((msg) => msg.skinType === activeType)
+            .map((msg, idx) => (
+              <div key={idx}>{msg.message}</div>
+            ))}
+        </div>
+
+        <div className="relative w-full h-[2px] bg-main-typeStroke">
+          {/* 기본 선 위에 겹치는 그라데이션 선 */}
+          <div
+            className="
+          absolute top-0 left-0 w-full h-full
+          bg-gradient-to-r from-main to-main-purple
+          opacity-0 group-hover:opacity-100
+          transition-opacity duration-500
+          "
+          ></div>
+        </div>
       </div>
     </div>
   );
