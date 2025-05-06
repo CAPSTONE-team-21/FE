4d75620f43f24d1c2b76b8827804b552ec3168a3
mikyung <seomk08@gmail.com>
2025-05-07 00:27:07 +0900

    refactor: LogoSection UI 완료

--- src/components/LoginPage/LogoSection.jsx
+++ src/components/LoginPage/LogoSection.jsx
@@ -3,9 +3,13 @@ import { Logo } from '../../utils/icons';
 
 const LogoSection = () => {
   return (
-    <>
-      <div>Logo</div>
-    </>
+    <div
+      className="flex space-x-[6px] border-red-600
+    mt-[80px] mb-[36px]"
+    >
+      <img className="w-[37px]" src={Iconspoid} alt="LogoImg" />
+      <img className="w-[138px]" src={Logo} alt="Logo" />
+    </div>
   );
 };
 export default LogoSection;
