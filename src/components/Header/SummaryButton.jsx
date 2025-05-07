1b7913b1fc961b8f4c730fc1e042e4806a78589e
장서현 <jsh030213@gmail.com>
2025-05-07 21:15:13 +0900

    style: 요약 버튼 UI 디자인

--- src/components/Header/SummaryButton.jsx
+++ src/components/Header/SummaryButton.jsx
@@ -1,9 +1,13 @@
-import { IconSummaryBlack } from '../../utils/icons';
+import { IconSummary } from '../../utils/icons';
 
 const SummaryButton = () => {
   return (
-    <div className="flex gap-[10px]">
-      <img className="w-[12px] h-auto" src={IconSummaryBlack} alt="filter 아이콘" />
+    <div
+      className="flex px-4 py-2 gap-[10px] items-center border border-gray-stroke05 rounded-[10px] text-[14px] font-medium
+    hover:border-gray-stroke10 hover:shadow-custom
+    transition-all duration-200 cursor-pointer"
+    >
+      <img className="w-[12px] h-auto" src={IconSummary} alt="filter 아이콘" />
       <div>요약</div>
     </div>
   );
