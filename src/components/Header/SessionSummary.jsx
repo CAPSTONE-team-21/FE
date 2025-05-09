26a2a3123f5666a68f07d21f1af08cf849a572c1
장서현 <jsh030213@gmail.com>
2025-05-09 17:44:37 +0900

    style: 색상 수정

--- src/components/Header/SessionSummary.jsx
+++ src/components/Header/SessionSummary.jsx
@@ -25,10 +25,10 @@ const SessionSummary = ({ onClick, sessionId }) => {
   }, [sessionId]);
 
   return (
-    <div className="flex justify-center fixed left-0 top-0 w-screen h-screen z-50 bg-black bg-opacity-[4%] ">
+    <div className="flex justify-center fixed left-0 top-0 w-screen h-screen z-50">
       {/* overlay */}
       <div className="fixed inset-0 bg-black animate-fadeIn"></div>
-      <div className="bg-white w-[760px] h-[512px] mt-[80px] rounded-[20px] border border-gray-stroke07 shadow-modal animate-modalIn ">
+      <div className="bg-white backdrop-blur-lg w-[760px] h-[512px] mt-[80px] rounded-[20px] border border-gray-stroke07 shadow-modal animate-modalIn ">
         <div className="flex items-center justify-between px-8 pt-6 pb-5 border-b border-gray-stroke05">
           {/* 모달창 상단 (취소 버튼) */}
           <div className="flex items-center gap-2 ">
