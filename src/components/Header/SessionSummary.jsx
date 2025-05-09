5e55be803df82e2ca3d6f8fba86b73e66f3de408
장서현 <jsh030213@gmail.com>
2025-05-09 16:30:58 +0900

    style: 상단 디자인 수정

--- src/components/Header/SessionSummary.jsx
+++ src/components/Header/SessionSummary.jsx
@@ -6,10 +6,10 @@ const SessionSummary = ({ onClick }) => {
     <div className="flex justify-center fixed left-0 top-0 w-screen h-screen z-50 bg-black bg-opacity-[4%] ">
       <div className="bg-white w-[760px] h-[512px] mt-[80px] rounded-[20px] border border-gray-stroke07 shadow-modal">
         {/* 모달창 상단 (취소 버튼) */}
-        <div className="flex justify-between px-8 py-6">
-          <di className="flex items-center gap-2">
+        <div className="flex items-center justify-between px-8 pt-6 pb-5 border-b border-gray-stroke05">
+          <di className="flex items-center gap-2 ">
             <img src={IconSummaryBlue} alt="요약" />
-            <span className="text-[18px] font-semibold text-main">현재 채팅방 내용 요약</span>
+            <span className="text-[18px] font-semibold text-main ">현재 채팅방 내용 요약</span>
           </di>
           <img onClick={onClick} className="cursor-pointer w-3" src={IconCancel} alt="취소" />
         </div>
