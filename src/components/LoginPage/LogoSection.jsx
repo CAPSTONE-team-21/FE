70922a39fa1ab0f3f53a8b55ba9040528cc9c098
mikyung <seomk08@gmail.com>
2025-05-21 10:55:14 +0900

    Refactor: 커서 포인터 추가 및 헤더, 로그인에서 로고 누르면 /로 이동하게 함

--- src/components/LoginPage/LogoSection.jsx
+++ src/components/LoginPage/LogoSection.jsx
@@ -1,11 +1,15 @@
+import { nav } from 'framer-motion/client';
 import { IconLogo } from '../../utils/icons';
 import { Logo } from '../../utils/icons';
+import { useNavigate } from 'react-router-dom';
 
 const LogoSection = () => {
+  const nav = useNavigate();
   return (
     <div
       className="flex space-x-[6px] border-red-600
-    mt-[80px] mb-[36px]"
+    mt-[80px] mb-[36px] cursor-pointer"
+      onClick={() => nav('/')}
     >
       <img className="w-[37px]" src={IconLogo} alt="LogoImg" />
       <img className="w-[138px]" src={Logo} alt="Logo" />
